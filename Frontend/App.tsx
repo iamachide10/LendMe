f254f7c1bdac591cde57743a2bcd6dcd921a3539
iamachide <iamachide10@gmail.com>
2026-06-26 22:07:48 +0000

    fix messages

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 import AppNavigator from './src/navigation/AppNavigator';
+import { StatusBar } from 'expo-status-bar';
 
 
 const App: React.FC = () => (
   <PaperProvider>
+    <StatusBar style='light' backgroundColor='#1a1a2e'/>
     <AppNavigator />
   </PaperProvider>
 
--- Frontend/src/utils/constants.ts
+++ Frontend/src/utils/constants.ts
@@ -1,6 +1,9 @@
-export const API_BASE_URL = 'http://10.144.178.200:8080/api';
-export const WEBSOCKET_URL = 'http://10.144.178.200:8080/ws';
-export const BASE_URL = 'http://10.144.178.200:8080';
+// export const BASE_URL = 'http://10.144.178.200:8080';
+// export const API_BASE_URL = 'http://10.144.178.200:8080/api';
+// export const WEBSOCKET_URL = 'http://10.144.178.200:8080/ws';
+export const WEBSOCKET_URL = 'https://lendme-kjm7.onrender.com/ws';
+export const API_BASE_URL = 'https://lendme-kjm7.onrender.com/api';
+export const BASE_URL = 'https://lendme-kjm7.onrender.com';
 export const ITEM_CATEGORIES = [
   'ELECTRONICS',
   'CLOTHING',
